e4afa4178d042210b69b8546ae0d709fd0899fc5
Magapo85 <petithomme.guillaume@gmail.com>
2022-07-20 23:22:13 +0200

    Epreuve Terre Exo 1

---
+++ terre01.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+texteAffiche=''
+i=0
+while i<alphabet.length()
+   texteAffiche=texteAffiche+alphabet[i]
+   i=i+1
+end
+texteAffiche=texteAffiche
+puts texteAffiche
+puts "\n"
